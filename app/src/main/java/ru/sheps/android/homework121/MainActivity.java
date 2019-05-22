e56df7844c487c735e8e4d1da66e7b15b95dbb56
Katya Sheps <katya@sheps.ru>
2019-05-22 22:22:25 +0300

    Layout grapfic part

--- app/src/main/java/ru/sheps/android/homework121/MainActivity.java
+++ app/src/main/java/ru/sheps/android/homework121/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //String strCatName = nickNameEditText.getText().toString(); // приводим к типу String
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
