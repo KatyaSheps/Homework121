19d978ee300b3935cf9623b2012490853fbf22c2
Katya Sheps <katya@sheps.ru>
2019-06-03 22:53:00 +0300

    Change tool for show message. At first was: Toast toast = Toast.makeText(getApplicationContext(), textMessage, Toast.LENGTH_SHORT); toast.setGravity(Gravity.CENTER, 0, 0); toast.show();
    I delete toast and set new textview:
    
    textView.setText(textMessage);

--- app/src/main/java/ru/sheps/android/homework121/MainActivity.java
+++ app/src/main/java/ru/sheps/android/homework121/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,14 +20,13 @@ public class MainActivity extends AppCompatActivity {
         final EditText email = findViewById(R.id.editEmail);
         Button btnOK = findViewById(R.id.btnOK);
         Button btnClear = findViewById(R.id.btnClear);
+        final TextView textView = findViewById(R.id.textMessenger);
 
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String textMessage =  getString(R.string.massage1) + " " + username.getText().toString() + " " + getString(R.string.massage2)+ " " +  email.getText().toString();
-                Toast toast = Toast.makeText(getApplicationContext(), textMessage, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
+                textView.setText(textMessage);
             }
         });
 
